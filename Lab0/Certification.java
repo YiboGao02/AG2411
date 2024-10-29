1013cc0874a9cb107d1aec6c0f21eef77de68303
YiboGao02 <yibo_gao@outlook.com>
2024-10-29 14:19:01 +0100

    add Lab0

---
+++ Lab0/Certification.java
@@ -0,0 +1,7 @@
+package Lab0;
+
+public class Certification {
+    public static void main(String[] args) {
+        System.out.print("I hereby certify that I have gone through pages 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, and 19 of this exercise.");
+    }
+}
